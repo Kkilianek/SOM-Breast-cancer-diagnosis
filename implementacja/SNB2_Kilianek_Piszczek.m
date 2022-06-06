5a090d5baa19386ddfb1d2088fd874a8d0cbcd38
Adam Piszczek <adampiszczek1904@gmail.com>
2022-06-06 14:55:16 +0200

    specyficznosc i czulosc

--- implementacja/SNB2_Kilianek_Piszczek.m
+++ implementacja/SNB2_Kilianek_Piszczek.m
@@ -282,7 +282,7 @@ for i=1:wt
     if wynik(I1,I2) == 1 
         liczbaZlosliwychtest = liczbaZlosliwychtest + 1;
         if zbiorTestowy(i,6) == 0
-            prawdziwieujemny = prawdziwieujemny + 1;
+            falszywiedodatni = falszywiedodatni + 1;
             liczniktest = liczniktest + 1;
         else
             prawdziwiedodatni = prawdziwiedodatni + 1;
@@ -293,7 +293,7 @@ for i=1:wt
             liczniktest = liczniktest + 1;
             falszywieujemny = falszywieujemny + 1;
         else
-            falszywiedodatni = falszywiedodatni + 1;
+            prawdziwieujemny = prawdziwieujemny + 1;
         end
     end
 end
